660d178e8929668cb686dbd81c6c536e2fcc23b7
ka1te6 <a.andrey2006@yandex.ru>
2025-05-11 13:59:09 +0300

    Добавлен новый формат через clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // подключено для работы с вводом/выводом
-//я уже убрал это ранее, поэтому просто оставлю комментарий
-int main(){
+// я уже убрал это ранее, поэтому просто оставлю комментарий
+int
+main()
+{
   std::string name;
   std::cin >> name;
   std::cout << "Hello, world from " << name << "!" << std::endl;
