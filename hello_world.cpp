d5161fb1fc7a4966a7e33f3ec110fc0c17b23e2d
ka1te6 <a.andrey2006@yandex.ru>
2025-05-11 13:47:37 +0300

    Убрать using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-
+//я уже убрал это ранее, поэтому просто оставлю комментарий
 int main(){
   std::string name;
   std::cin >> name;
-  std::cout << "Hello, world from " << name << "!" << std::endl;
-  return 0;
-}
