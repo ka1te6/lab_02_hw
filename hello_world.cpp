f77c217fd450ab1923486a37f4e080f6686d56f1
ka1te6 <a.andrey2006@yandex.ru>
2025-05-11 13:45:48 +0300

    Добавлен ввод имени

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(){
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!" << std::endl;
+  return 0;
 }
